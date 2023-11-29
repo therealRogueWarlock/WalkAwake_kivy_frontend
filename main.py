7521b4cfdf037c11c640d0e30838657fbd131494
Andreas <42655737+anonisnap@users.noreply.github.com>
2023-11-29 19:43:57 +0100

    Possible fix for screen

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ kivy.require('1.0.7')
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.app import App
 
+from kivy.core.window import Window
+
+Window.fullscreen = true
+Window.always_on_top = true
+Window.show_cursor = false
 
 class MainApp(App):
 

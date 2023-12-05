f742df60bf070fc90a85a47a58229afdadfd31a9
Andreas <42655737+anonisnap@users.noreply.github.com>
2023-12-05 15:12:09 +0100

    Home is pretty

--- views/home.py
+++ views/home.py
@@ -3,38 +3,36 @@ from kivy.uix.screenmanager import Screen
 from kivy.clock import Clock
 from kivy import utils
 
-
 class Home(Screen):
     name = 'home'
-    color = utils.get_color_from_hex('#3E8BAF')
 
     def __init__(self, **kw):
         super().__init__(**kw)
-        
+
         # Set initial values
         self.set_next_alarm()
         self.set_date()
         self.set_time()
-        
+
         # Schedule for Updates
         Clock.schedule_interval(lambda dt: self.set_date(), 300)
         Clock.schedule_interval(lambda dt: self.set_time(), 1)
-                
+
     def Settings() -> None:
         print('Going to Settings')
         return
-        
+
     #
     def set_date(self) -> None:
         today = date.today()
         self.ids.Date.text = today.strftime("%d %b, %Y")
         return
-    
+
     def set_time(self) -> None:
         now = datetime.now()
         self.ids.CurrentTime.text = now.strftime("%H:%M")
         return
-    
+
     def set_next_alarm(self) -> None:
         # Get Alarm for Tomorrow if one is made
         alarm_tomorrow = '' or 'None Tomorrow'

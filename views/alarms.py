dc0fdeef37b1b07c5c5e87c40d33d1ae7a7fb928
Andreas <42655737+anonisnap@users.noreply.github.com>
2023-12-05 16:35:43 +0100

    alarms

--- views/alarms.py
+++ views/alarms.py
@@ -3,4 +3,11 @@ from kivy.uix.screenmanager import Screen
 
 class Alarms(Screen):
     name = 'alarms'
-    pass
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ids.Header.text = 'Alarms'
+
+    def go_to(self, day: str) -> None:
+        print(f'Going to: {screen}')
+        self.manager.current = 'alarm'

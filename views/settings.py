ee6f4019095491125af196a980464d99ccd46f1c
Andreas <42655737+anonisnap@users.noreply.github.com>
2023-12-05 16:00:56 +0100

    Settings finished

--- views/settings.py
+++ views/settings.py
@@ -4,12 +4,8 @@ from kivy.uix.screenmanager import Screen
 class Settings(Screen):
     name = 'settings'
 
-    def on_pre_enter(self):
-        print("pre enter settings")
-
     def __init__(self, **kw):
         super().__init__(**kw)
 
-        # Set initial values
-
-        # Schedule for Updates
+    def on_pre_enter(self):
+        print("Maybe preload Settings?")

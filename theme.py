1e46f94972dce63a2ed501b5927c7292c0bee8e1
Andreas <42655737+anonisnap@users.noreply.github.com>
2023-12-05 15:56:29 +0100

    added invisible

--- theme.py
+++ theme.py
@@ -1,9 +1,13 @@
 class Colour():
     BACKGROUND = (.24, .55, .69)
-    PRIMARY    = (.00, .00, .00)
-    ACCENT     = (.33, .20, .15)
-    ENABLED    = (.24, .55, .69)
-    DISABLED   = (.24, .55, .69) 
+    PRIMARY = (.00, .00, .00)
+    ACCENT = (.33, .20, .15)
+    
+    ENABLED = (.24, .55, .69)
+    DISABLED = (.24, .55, .69)
+    
+    INVISIBLE = (0, 0, 0, 0,)
+
 
 class Text():
     XL = '72sp'
@@ -12,18 +16,18 @@ class Text():
     SM = '18sp'
 
 
-
 class Size():
-    WIDTH  = 800
+    WIDTH = 800
     HEIGHT = 480
-    
+
+
 class Icons():
-    HOME     = './views/icons/home.png'
-    BACK     = './views/icons/back_arrow.png'
+    HOME = './views/icons/home.png'
+    BACK = './views/icons/back_arrow.png'
     SETTINGS = './views/icons/settings.png'
-    
+
     CONFIRM = './views/icons/plus.png'
-    CANCEL  = './views/icons/cancel.png'
-    
-    PLUS      = './views/icons/plus.png'
+    CANCEL = './views/icons/cancel.png'
+
+    PLUS = './views/icons/plus.png'
     SOUND_OFF = './views/icons/sound_off.png'

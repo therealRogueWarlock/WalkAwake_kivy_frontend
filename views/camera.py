6b3343fdd98a3a0af75d2a55c5fa23970be07a6b
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2023-12-11 16:02:27 +0100

    Update camera.py

--- views/camera.py
+++ views/camera.py
@@ -33,7 +33,7 @@ class Camera(Screen):
         # call model layer to capture image
         self.loop.cancel()
         result = self.computer_vision_manager.VerifyImage("sink", self.capture_path)
-        print("Result from verify" + str(result))
+        print("Result from verify " + str(result))
         self.ids.imageView.source = ""
         self.ids.imageView.source = self.capture_path + "/image_capture.jpg"
 

447f3f0e6aefceb41af3c487d1bd112cc6c1ffe0
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2023-12-11 17:29:00 +0100

    Update camera.py

--- views/camera.py
+++ views/camera.py
@@ -1,5 +1,6 @@
 from kivy.uix.screenmanager import Screen
 from kivy.clock import Clock
+from threading import Thread
 from theme import Icons
 from WalkAwake.CameraModule import ComputerVisionManager
 
@@ -21,27 +22,29 @@ class Camera(Screen):
         self.img_counter = self.img_counter_start
         self.img_counter_max = 303
 
-    def on_pre_enter(self):
+    def on_enter(self):
         self.target = self.targets[1]
         self.ids.TargetText.text = "Target: " + self.target
         # Start Camera Scheduler
         self.ids.ImageView.source = self.capture_path + "/image_capture.jpg"
         #self.computer_vision_manager.Start(self.feed_path)
-        #self.loop = Clock.schedule_interval(lambda dt: self.update_image_view(), 1/self.FPS)
+        self.loop = Clock.schedule_interval(lambda dt: self.update_image_view(), 1)
 
-    def on_pre_leave(self):
+
+    def on_leave(self):
         # Stop Camera Scheduler
         #self.computer_vision_manager.Stop()
-        pass
+        self.loop.cancel()
 
-    def capture(self):
-        # call model layer to capture image
-        #self.loop.cancel()
+    def verify_image(self):
         result = self.computer_vision_manager.VerifyImage(self.target, self.capture_path)
         print("Result from verify " + str(result))
 
-        self.ids.ImageView.source = Icons.ADD
-        self.ids.ImageView.source = self.capture_path + "/image_capture.jpg"
+    def capture(self):
+        # call model layer to capture image
+        thread = Thread(target=self.verify_image, daemon=True)
+        thread.start()
+        print("after thread")
 
     def update_image_view(self):
-        pass
+        self.ids.ImageView.reload()
